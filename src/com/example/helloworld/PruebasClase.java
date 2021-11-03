9ebd036ff8ec3de2c187868a772790c3d4d2a7e2
Alejandrogaralv <alejandrogaralv448@gmail.com>
2021-11-03 16:52:42 +0100

    Commit prueba casa

--- src/com/example/helloworld/PruebasClase.java
+++ src/com/example/helloworld/PruebasClase.java
@@ -2,6 +2,6 @@ package com.example.helloworld;
 
 public class PruebasClase {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        System.out.println("Pruebas Casa!!!");
     }
 }
